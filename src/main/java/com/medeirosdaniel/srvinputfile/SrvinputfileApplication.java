50a4adb2392c627aa2fd9555d4a5766721ad1780
daniel-medeirossp <daniel.medeirossp@gmail.com>
2020-02-11 10:49:40 -0300

    Comentado POM - MYSQL e Alterado porta para 9123 no application.properties

---
+++ src/main/java/com/medeirosdaniel/srvinputfile/SrvinputfileApplication.java
@@ -0,0 +1,13 @@
+package com.medeirosdaniel.srvinputfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvinputfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvinputfileApplication.class, args);
+	}
+
+}
