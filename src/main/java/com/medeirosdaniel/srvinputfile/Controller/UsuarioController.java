8a2732cf73a767490c7763c75a9f4e32f1b78e73
daniel-medeirossp <daniel.medeirossp@gmail.com>
2020-02-14 16:50:25 -0300

    Descomentado as classes comentadas para mandar para Matheus

--- src/main/java/com/medeirosdaniel/srvinputfile/Controller/UsuarioController.java
+++ src/main/java/com/medeirosdaniel/srvinputfile/Controller/UsuarioController.java
@@ -1,79 +1,79 @@
 package com.medeirosdaniel.srvinputfile.Controller;
 
-//import com.medeirosdaniel.srvinputfile.Entity.UsuarioEntity;
+import com.medeirosdaniel.srvinputfile.Entity.UsuarioEntity;
 import com.medeirosdaniel.srvinputfile.Security.EncriptaSecurity;
 import com.medeirosdaniel.srvinputfile.Security.ValidaLoginSecurity;
 import com.medeirosdaniel.srvinputfile.Service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-//
-//import javax.persistence.EntityManager;
-//import javax.transaction.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
 
-//    @Autowired
-//    private UsuarioService usuarioService;
-//
-//    @GetMapping
-//    public List<UsuarioEntity> buscaTodosOsUsuarios() {
-//        return usuarioService.buscaTodosOsUsuarios();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public UsuarioEntity buscaUsuarioPorId(@PathVariable("id") Long id) {
-//        return usuarioService.buscaUsuarioPorId(id);
-//    }
-//
-//    @GetMapping("/email/{email}")
-//    public UsuarioEntity buscaUsuarioPorEmail(@PathVariable("email") String email) {
-//        return usuarioService.buscaUsuarioPorEmail(email);
-//    }
-//
-//    @GetMapping("/celular/{celular}")
-//    public UsuarioEntity buscaUsuarioPorCelular(@PathVariable("celular") String celular) {
-//        return usuarioService.buscaUsuarioPorCelular(celular);
-//    }
-//
-//    @GetMapping("/encoder/{palavra}/{email}")
-//    public Boolean encoderPalavra(@PathVariable("palavra")String palavra,@PathVariable("email")String email) {
-//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//        UsuarioEntity testa = usuarioService.buscaUsuarioPorEmail(email);
-//        String x = testa.getSenha();
-//        ValidaLoginSecurity n = new ValidaLoginSecurity();
-//        return n.validaLoginDoUsuario(palavra,x);
-//    }
-//
-//    @PostMapping("/{nome}/{sobre}/{email}/{celular}/{senha}")
-//    public UsuarioEntity adicionaNovoUsuario(@PathVariable("nome") String nome, @PathVariable("sobre") String sobre, @PathVariable("email") String email,
-//                                             @PathVariable("celular") String celular, @PathVariable("senha") String senha) {
-//        EncriptaSecurity es = new EncriptaSecurity();
-//        UsuarioEntity novo = new UsuarioEntity();
-//        novo.setNome(nome.toUpperCase());
-//        novo.setSobrenome(sobre.toUpperCase());
-//        novo.setEmail(email.toLowerCase());
-//        novo.setCelular(celular);
-//        novo.setSenha(es.encripta(senha));
-//        return usuarioService.adicionaNovoUsuario(novo);
-//    }
-//
-//
-//    @PutMapping("/{nome}/{sobre}/{email}/{celular}/{senha}")
-//    public UsuarioEntity alteraDadosDoUsuarioPorEmail(@PathVariable("nome") String nome, @PathVariable("sobre") String sobre, @PathVariable("email") String email,
-//                                                      @PathVariable("celular") String celular, @PathVariable("senha") String senha) {
-//        UsuarioEntity dados = new UsuarioEntity();
-//        dados.setNome(nome);
-//        dados.setSobrenome(sobre);
-//        dados.setEmail(email);
-//        dados.setCelular(celular);
-//        dados.setSenha(senha);
-//        return usuarioService.atualizaUsuario(dados);
-//
-//    }
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<UsuarioEntity> buscaTodosOsUsuarios() {
+        return usuarioService.buscaTodosOsUsuarios();
+    }
+
+    @GetMapping("/{id}")
+    public UsuarioEntity buscaUsuarioPorId(@PathVariable("id") Long id) {
+        return usuarioService.buscaUsuarioPorId(id);
+    }
+
+    @GetMapping("/email/{email}")
+    public UsuarioEntity buscaUsuarioPorEmail(@PathVariable("email") String email) {
+        return usuarioService.buscaUsuarioPorEmail(email);
+    }
+
+    @GetMapping("/celular/{celular}")
+    public UsuarioEntity buscaUsuarioPorCelular(@PathVariable("celular") String celular) {
+        return usuarioService.buscaUsuarioPorCelular(celular);
+    }
+
+    @GetMapping("/encoder/{palavra}/{email}")
+    public Boolean encoderPalavra(@PathVariable("palavra")String palavra,@PathVariable("email")String email) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        UsuarioEntity testa = usuarioService.buscaUsuarioPorEmail(email);
+        String x = testa.getSenha();
+        ValidaLoginSecurity n = new ValidaLoginSecurity();
+        return n.validaLoginDoUsuario(palavra,x);
+    }
+
+    @PostMapping("/{nome}/{sobre}/{email}/{celular}/{senha}")
+    public UsuarioEntity adicionaNovoUsuario(@PathVariable("nome") String nome, @PathVariable("sobre") String sobre, @PathVariable("email") String email,
+                                             @PathVariable("celular") String celular, @PathVariable("senha") String senha) {
+        EncriptaSecurity es = new EncriptaSecurity();
+        UsuarioEntity novo = new UsuarioEntity();
+        novo.setNome(nome.toUpperCase());
+        novo.setSobrenome(sobre.toUpperCase());
+        novo.setEmail(email.toLowerCase());
+        novo.setCelular(celular);
+        novo.setSenha(es.encripta(senha));
+        return usuarioService.adicionaNovoUsuario(novo);
+    }
+
+
+    @PutMapping("/{nome}/{sobre}/{email}/{celular}/{senha}")
+    public UsuarioEntity alteraDadosDoUsuarioPorEmail(@PathVariable("nome") String nome, @PathVariable("sobre") String sobre, @PathVariable("email") String email,
+                                                      @PathVariable("celular") String celular, @PathVariable("senha") String senha) {
+        UsuarioEntity dados = new UsuarioEntity();
+        dados.setNome(nome);
+        dados.setSobrenome(sobre);
+        dados.setEmail(email);
+        dados.setCelular(celular);
+        dados.setSenha(senha);
+        return usuarioService.atualizaUsuario(dados);
+
+    }
 
 
 }
--- src/main/java/com/medeirosdaniel/srvinputfile/Service/UsuarioService.java
+++ src/main/java/com/medeirosdaniel/srvinputfile/Service/UsuarioService.java
@@ -1,75 +1,75 @@
 package com.medeirosdaniel.srvinputfile.Service;
 
-//import com.medeirosdaniel.srvinputfile.Entity.UsuarioEntity;
-//import com.medeirosdaniel.srvinputfile.Repository.UsuarioRepository;
+import com.medeirosdaniel.srvinputfile.Entity.UsuarioEntity;
+import com.medeirosdaniel.srvinputfile.Repository.UsuarioRepository;
 import com.medeirosdaniel.srvinputfile.Security.EncriptaSecurity;
 import com.medeirosdaniel.srvinputfile.Security.ValidaLoginSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
-//import javax.transaction.Transactional;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UsuarioService {
 
-//    @Autowired
-//    private UsuarioRepository usuarioRepository;
-//
-//    public List<UsuarioEntity> buscaTodosOsUsuarios() {
-//        return usuarioRepository.findAll();
-//    }
-//
-//    public UsuarioEntity buscaUsuarioPorId(Long id){
-//        Optional<UsuarioEntity> busca = usuarioRepository.findById(id);
-//        UsuarioEntity resultado = new UsuarioEntity();
-//        resultado.setId(busca.get().getId());
-//        resultado.setNome(busca.get().getNome());
-//        resultado.setSobrenome(busca.get().getSobrenome());
-//        resultado.setEmail(busca.get().getEmail());
-//        resultado.setCelular(busca.get().getCelular());
-//        resultado.setSenha(busca.get().getSenha());
-//        return resultado;
-//    }
-//
-//    public UsuarioEntity buscaUsuarioPorEmail(String email){
-//        return usuarioRepository.buscaUsuarioPorEmail(email);
-//    }
-//
-//    public UsuarioEntity buscaUsuarioPorCelular(String celular){
-//        return usuarioRepository.buscaUsuarioPorCelular(celular);
-//    }
-//
-//    public UsuarioEntity adicionaNovoUsuario(UsuarioEntity usuarioEntity) {
-//        return usuarioRepository.save(usuarioEntity);
-//    }
-//
-//    @Transactional
-//    public UsuarioEntity atualizaUsuario(UsuarioEntity usuarioEntity){
-//        UsuarioEntity dadosAntigos = new UsuarioEntity();
-//        dadosAntigos = usuarioRepository.buscaUsuarioPorEmail(usuarioEntity.getEmail());
-//
-//        ValidaLoginSecurity vls = new ValidaLoginSecurity();
-//
-//        if (vls.validaLoginDoUsuario(usuarioEntity.getSenha(), dadosAntigos.getSenha())) {
-//            EncriptaSecurity es = new EncriptaSecurity();
-//            UsuarioEntity atualizaDados = new UsuarioEntity();
-//            atualizaDados.setId(dadosAntigos.getId());
-//            atualizaDados.setNome(usuarioEntity.getNome().toUpperCase());
-//            atualizaDados.setSobrenome(usuarioEntity.getSobrenome().toUpperCase());
-//            atualizaDados.setEmail(usuarioEntity.getEmail());
-//            atualizaDados.setCelular(usuarioEntity.getCelular());
-//            atualizaDados.setSenha(dadosAntigos.getSenha());
-//            usuarioRepository.flush();
-//            return atualizaDados;
-//        } else {
-//            return new UsuarioEntity();
-//        }
-//
-//
-//    }
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<UsuarioEntity> buscaTodosOsUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    public UsuarioEntity buscaUsuarioPorId(Long id){
+        Optional<UsuarioEntity> busca = usuarioRepository.findById(id);
+        UsuarioEntity resultado = new UsuarioEntity();
+        resultado.setId(busca.get().getId());
+        resultado.setNome(busca.get().getNome());
+        resultado.setSobrenome(busca.get().getSobrenome());
+        resultado.setEmail(busca.get().getEmail());
+        resultado.setCelular(busca.get().getCelular());
+        resultado.setSenha(busca.get().getSenha());
+        return resultado;
+    }
+
+    public UsuarioEntity buscaUsuarioPorEmail(String email){
+        return usuarioRepository.buscaUsuarioPorEmail(email);
+    }
+
+    public UsuarioEntity buscaUsuarioPorCelular(String celular){
+        return usuarioRepository.buscaUsuarioPorCelular(celular);
+    }
+
+    public UsuarioEntity adicionaNovoUsuario(UsuarioEntity usuarioEntity) {
+        return usuarioRepository.save(usuarioEntity);
+    }
+
+    @Transactional
+    public UsuarioEntity atualizaUsuario(UsuarioEntity usuarioEntity){
+        UsuarioEntity dadosAntigos = new UsuarioEntity();
+        dadosAntigos = usuarioRepository.buscaUsuarioPorEmail(usuarioEntity.getEmail());
+
+        ValidaLoginSecurity vls = new ValidaLoginSecurity();
+
+        if (vls.validaLoginDoUsuario(usuarioEntity.getSenha(), dadosAntigos.getSenha())) {
+            EncriptaSecurity es = new EncriptaSecurity();
+            UsuarioEntity atualizaDados = new UsuarioEntity();
+            atualizaDados.setId(dadosAntigos.getId());
+            atualizaDados.setNome(usuarioEntity.getNome().toUpperCase());
+            atualizaDados.setSobrenome(usuarioEntity.getSobrenome().toUpperCase());
+            atualizaDados.setEmail(usuarioEntity.getEmail());
+            atualizaDados.setCelular(usuarioEntity.getCelular());
+            atualizaDados.setSenha(dadosAntigos.getSenha());
+            usuarioRepository.flush();
+            return atualizaDados;
+        } else {
+            return new UsuarioEntity();
+        }
+
+
+    }
 
 
 }
